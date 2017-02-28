c9212a933ca1885266951ab0f38f9151b7c430e1
Linnu <linda.tanissaar@khk.ee>
2017-02-28 14:56:35 +0200

    Validating enterants' login form

--- views/forms/forms_index.php
+++ views/forms/forms_index.php
@@ -1,3 +1,61 @@
+<style>
+    input[name="firstName"], input[name="lastName"]{
+        text-transform: capitalize;
+    }
+</style>
+
+<script>
+    var id_length;
+    var validate = function(){
+        invalid=0;
+        id_length = document.myForm.pin.value.length;
+
+        // firstName validation
+        if(document.myForm.firstName.value==""){
+            alert("Eesnime lahter peab olema täidetud!");
+            invalid++;
+        }
+
+        if(!/^[-\sa-zA-Z]+$/.test(document.myForm.firstName.value)) {
+            alert("Eesnimi võib sisaldada ainult tähti, tühikut ja sidekriipsu!");
+            return;
+        }
+
+        // lastName validation
+        if(document.myForm.lastName.value=="") {
+            alert("Perenime lahter peab olema täidetud!");
+            invalid++;
+        }
+
+        if(!/^[-\sa-zA-Z]+$/.test(document.myForm.lastName.value)) {
+            alert("Perenimi võib sisaldada ainult tähti, tühikut ja sidekriipsu!");
+            return;
+        }
+
+        // id validation
+        if(document.myForm.pin.value==""){
+            alert("Isikukoodi lahter peab olema täidetud!");
+            invalid++;
+        }
+
+        if(id_length != 11) {
+            alert("Sisestatud ID-s pole õige arv numbreid!");
+            invalid++;
+        }
+
+        if(document.myForm.pin.value.match(/^[0-9]+$/) == null) {
+            alert("Sisestasid mitte numbri!");
+        }
+
+        // final validation
+        if(invalid!=0){
+            return false;
+        }else{
+            return true;
+        }
+    }
+</script>
+
 <p>Siin keskkonnas saad Sa testida oma CSS'i ja HTML-i oskusi. Test koosneb teoreetilisest ja praktilisest osast. Teoreetilises osas tuleb Sul vastata kümnele küsimusele.
     Tegemist on valikvastustega küsimustega, kus ainult üks vastus on õige. Pärast teoreetilist osa tuleb Sul lahendada praktiline ülesanne.</p>
 
@@ -16,21 +74,21 @@
 
             <!-- body (form) -->
             <div class="modal-body">
-                <form role="form" action="insert.php" method="post">
+                <form role="form" action="insert.php" method="post" name="myForm">
                     <div class="form-group">
-                        <input type="text" class="form-control" placeholder="Eesnimi" name="eesnimi">
+                        <input type="text" class="form-control" placeholder="Eesnimi" name="firstName" >
+                    </div>
+                    <script></script>
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Perenimi" name="lastName">
                     </div>
                     <div class="form-group">
-                        <input type="text" class="form-control" placeholder="Perenimi" name="perenimi">
-                    </div>
-                    <div class="form-group">
-                        <input type="text" class="form-control" placeholder="Isikukood" name="isikukood">
+                        <input type="text" class="form-control" placeholder="Isikukood" name="pin">
                     </div>
                 </form>
             </div>
 
             <!-- button -->
             <div class="modal-footer">
-                <button class="btn btn-primary btn-block">Edasta!</button>
+                <button class="btn btn-primary btn-block" onClick="return validate()">Edasta!</button>
             </div>
-
