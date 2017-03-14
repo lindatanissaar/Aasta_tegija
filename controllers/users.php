1eade57be0a9687391d7c9dba68b628551f07e02
Linnu <linda.tanissaar@khk.ee>
2017-03-14 20:59:17 +0200

    Administraatorina saan muuta, kustutada, lisada küsimusi ja pärast muutmist ka salvestada neid.

--- controllers/users.php
+++ controllers/users.php
@@ -56,47 +56,160 @@ class users extends Controller
 
     }
 
-    function AJAX_addingQuestions()
-
+    function AJAX_selectingQuestions()
     {
-        if (isset($_POST["question_text"]) && isset($_POST["answer_text_right"]) && isset($_POST["answer_text_false1"]) && isset($_POST["answer_text_false2"])) {
-            $question_text = $_POST["question_text"];
-            $answer_text_right = $_POST["answer_text_right"];
-            $answer_text_false1 = $_POST["answer_text_false1"];
-            $answer_text_false2 = $_POST["answer_text_false2"];
-            $conn = mysqli_connect("127.0.0.1", "root", "", "Aasta_tegija");
-
-
-            insert('questions',[
-                'text'=>$question_text
-            ]);
-
-            //$question_id = "SELECT question_id FROM questions WHERE text='$question_text'";
-            $question_id = mysqli_insert_id($conn);
-            insert('answers',[
-                'question_id' =>$question_id,
-                'answer_text'=>$answer_text_right,
-                'answer'=>1
-            ]);
-
-            insert('answers',[
-                'question_id' =>$question_id,
-                'answer_text'=>$answer_text_false1,
-                'answer'=>0
-            ]);
-
-            insert('answers',[
-                'question_id' =>$question_id,
-                'answer_text'=>$answer_text_false2,
-                'answer'=>0
-            ]);
-
-            echo "success";
+        $conn = mysqli_connect("127.0.0.1", "root", "", "Aasta_tegija");
+        $output = '';
+        $mysql = "SELECT * FROM questions ORDER BY question_id DESC";
+        $result = mysqli_query($conn, $mysql);
+        $output .= '  
+              <div class="table-responsive">  
+                   <table class="table table-bordered" width="80%">  
+                        <tr>  
+                             <th width="15%">Id</th>  
+                             <th width="70%">Tekst</th>  
+                             <th width="15%">Kustuta/Lisa</th>  
+                        </tr>';
+        if (mysqli_num_rows($result) > 0) {
+            while ($row = mysqli_fetch_array($result)) {
+                $output .= '  
+                        <tr>  
+                             <td>' . $row["question_id"] . '</td>  
+                             <td class="text" data-id1="' . $row["question_id"] . '" contenteditable>' . $row["text"] . '</td>  
+                             <td><button type="button" name="delete_btn" data-id3="' . $row["question_id"] . '" class="btn btn-xs btn-danger btn_delete2" style="background-color: #FFE600; border-color: #FFE600">x</button></td>  
+                        </tr>  
+                   ';
+            }
+            $output .= '  
+                   <tr>  
+                        <td></td>  
+                        <td id="text" contenteditable></td> 
+                        <td><button type="button" name="btn_add" id="btn_add2" class="btn btn-xs btn-success" style="background-color: #0054a6; border-color: #0054a6" >+</button></td>  
+                   </tr>  
+              ';
+        } else {
+            $output .= '<tr>  
+                                  <td colspan="4">Andmed puuduvad</td>  
+                             </tr>';
         }
+        $output .= '</table>  
+              </div>';
+        echo $output;
 
     }
 
+    function AJAX_insertingQuestions(){
+        $conn = mysqli_connect("127.0.0.1", "root", "", "Aasta_tegija");
+        $mysql = "INSERT INTO questions(text) VALUES('".$_POST["text"]."')";
+        if(mysqli_query($conn, $mysql))
+        {
+            echo 'Data Inserted';
+        }
+    }
 
+    function AJAX_editingQuestions(){
+        $conn = mysqli_connect("127.0.0.1", "root", "", "Aasta_tegija");
+        $id = $_POST["id"];
+        $text = $_POST["text"];
+        $column_name = $_POST["column_name"];
+        $mysql = "UPDATE questions SET ".$column_name."='".$text."' WHERE question_id='".$id."'";
+        if(mysqli_query($conn, $mysql))
+        {
+            echo 'Data Updated';
+        }
+    }
+
+    function AJAX_deletingQuestions(){
+        $conn = mysqli_connect("127.0.0.1", "root", "", "Aasta_tegija");
+        $mysql = "DELETE FROM questions WHERE question_id = '".$_POST["id"]."'";
+        if(mysqli_query($conn, $mysql))
+        {
+            echo 'Data Deleted';
+        }
+    }
+
+    // answers
+
+    function AJAX_selectingAnswers()
+    {
+        $conn = mysqli_connect("127.0.0.1", "root", "", "Aasta_tegija");
+        $output = '';
+        $mysql = "SELECT * FROM answers ORDER BY answer_id DESC";
+        $result = mysqli_query($conn, $mysql);
+        $output .= '  
+              <div class="table-responsive">  
+                   <table class="table table-bordered" width="80%">  
+                        <tr>  
+                             <th width="20%">Id</th>  
+                             <th width="20%">Küsimuse_id</th>  
+                             <th width="20%">Tekst</th>
+                             <th width="20%">Õige/väär</th>
+                             <th width="20%">Kustuta/Lisa</th>  
+                        </tr>';
+        if (mysqli_num_rows($result) > 0) {
+            while ($row = mysqli_fetch_array($result)) {
+                $output .= '  
+                        <tr>  
+                             <td>' . $row["answer_id"] . '</td>  
+                             <td class="question_id" data-id1="' . $row["answer_id"] . '" contenteditable>' . $row["question_id"] . '</td> 
+                             <td class="answer_text" data-id2="' . $row["answer_id"] . '" contenteditable>' . $row["answer_text"] . '</td>
+                             <td class="answer" data-id3="' . $row["answer_id"] . '" contenteditable>' . $row["answer"] . '</td>
+                             <td><button type="button" name="delete_btn" data-id4="' . $row["answer_id"] . '" class="btn btn-xs btn-danger btn_delete3" style="background-color: #FFE600; border-color: #FFE600">x</button></td>  
+                        </tr>  
+                   ';
+            }
+            $output .= '  
+                   <tr>  
+                        <td></td>
+                        <td id="question_id" contenteditable></td>
+                        <td id="answer_text" contenteditable></td>
+                        <td id="answer" contenteditable></td> 
+                        <td><button type="button" name="btn_add" id="btn_add3" class="btn btn-xs btn-success" style="background-color: #0054a6; border-color: #0054a6" >+</button></td>  
+                   </tr>  
+              ';
+        } else {
+            $output .= '<tr>  
+                                  <td colspan="4">Andmed puuduvad</td>  
+                             </tr>';
+        }
+        $output .= '</table>  
+              </div>';
+        echo $output;
+
+    }
+
+    function AJAX_insertingAnswers(){
+        $conn = mysqli_connect("127.0.0.1", "root", "", "Aasta_tegija");
+        $mysql = "INSERT INTO answers(question_id, answer_text, answer) VALUES('".$_POST["question_id"]."', '".$_POST["answer_text"]."', '".$_POST["answer"]."')";
+        if(mysqli_query($conn, $mysql))
+        {
+            echo 'Data Inserted';
+        }
+    }
+
+    function AJAX_editingAnswers(){
+        $conn = mysqli_connect("127.0.0.1", "root", "", "Aasta_tegija");
+        $id = $_POST["id"];
+        $text = $_POST["text"];
+        $column_name = $_POST["column_name"];
+        $mysql = "UPDATE answers SET ".$column_name."='".$text."' WHERE answer_id='".$id."'";
+        if(mysqli_query($conn, $mysql))
+        {
+            echo 'Data Updated';
+        }
+    }
+
+    function AJAX_deletingAnswers(){
+        $conn = mysqli_connect("127.0.0.1", "root", "", "Aasta_tegija");
+        $mysql = "DELETE FROM answers WHERE answer_id = '".$_POST["id"]."'";
+        if(mysqli_query($conn, $mysql))
+        {
+            echo 'Data Deleted';
+        }
+    }
+
+
+    // practical
     function AJAX_selectingPractical()
     {
         $conn = mysqli_connect("127.0.0.1", "root", "", "Aasta_tegija");
--- views/users/users_index.php
+++ views/users/users_index.php
@@ -163,75 +163,181 @@
     </script>
 
 
-
-<button type="button" class="btn btn-lg" data-toggle="modal" data-target="#popUpWindow3">Lisa küsimus</button>
-
-<div class="modal fade" id="popUpWindow3">
-    <div class="modal-dialog">
-        <div class="modal-content">
-
-            <!-- header -->
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal">&times;</button>
-                <h3 class="modal-title">Küsimuse lisamine</h3>
-                <p id="alertText1"></p>
-            </div>
-
-            <!-- body (form) -->
-            <div class="modal-body">
-                <form role="form"  name="myForm">
-                    <div class="form-group">
-                        <label for="question">Küsimus:</label>
-                        <textarea class="form-control" rows="5" id="question_text" name="question_text"></textarea>
-                    </div>
-                    <div class="form-group">
-                        <label for="right_answer">Õige vastus:</label>
-                        <textarea class="form-control" rows="3" id="answer_text_right" name="answer_text_right"></textarea>
-                    </div>
-                    <div class="form-group">
-                        <label for="false_answer_1">Vale vastus:</label>
-                        <textarea class="form-control" rows="3" id="answer_text_false1" name="answer_text_false1"></textarea>
-                    </div>
-                    <div class="form-group">
-                        <label for="false_answer_2">Vale vastus:</label>
-                        <textarea class="form-control" rows="3" id="answer_text_false2" name="answer_text_false2"></textarea>
-                    </div>
-                </form>
-            </div>
-
-            <!-- button -->
-            <div class="modal-footer">
-                <input type="button" id="btnSubmit3" class="btn btn-primary btn-block" value="Lisa uus küsimus">
-            </div>
+        <div class="table-responsive">
+            <h3 align="center">Lisa, kustuta ja muuda küsimust</h3><br />
+            <div id="live_data2"></div>
         </div>
-    </div>
-</div>
 
-    <script>
-        $(function () {
-            $('#btnSubmit3').click(function () {
-                var question_text = $("#question_text").val();
-                var answer_text_right = $("#answer_text_right").val();
-                var answer_text_false1 = $("#answer_text_false1").val();
-                var answer_text_false2 = $("#answer_text_false2").val();
+        <script>
+            $(document).ready(function(){
+                function fetch_data()
+                {
+                    $.ajax({
+                        url:"users/selectingQuestions",
+                        method:"POST",
+                        success:function(data){
+                            $('#live_data2').html(data);
+                        }
+                    });
+                }
+                fetch_data();
+                $(document).on('click', '#btn_add2', function(){
+                    var text = $('#text').text();
+                    if(text == '')
+                    {
+                        alert("Enter text");
+                        return false;
+                    }
 
+                    $.ajax({
+                        url:"users/insertingQuestions",
+                        method:"POST",
+                        data:{text:text},
+                        dataType:"text",
+                        success:function(data)
+                        {
+                            alert(data);
+                            fetch_data();
+                        }
+                    })
+                });
+                function edit_data(id, text, column_name)
+                {
+                    $.ajax({
+                        url:"users/editingQuestions",
+                        method:"POST",
+                        data:{id:id, text:text, column_name:column_name},
+                        dataType:"text",
+                        success:function(data){
+                            alert(data);
+                        }
+                    });
+                }
+                $(document).on('blur', '.text', function(){
+                    var id = $(this).data("id1");
+                    var text = $(this).text();
+                    edit_data(id, text, "text");
+                });
 
-                $.post("users/addingQuestions", {
-                    question_text: question_text,
-                    answer_text_right: answer_text_right,
-                    answer_text_false1: answer_text_false1,
-                    answer_text_false2: answer_text_false2
-                }).done(function (data) {
-                    if (data == "success") {
-                        //location.href = "users"
-                        document.getElementById('alertText1').innerHTML = "Uus küsimus lisatud"
-                    } else {
-                        alert("Error!")
+                $(document).on('click', '.btn_delete2', function(){
+                    var id=$(this).data("id3");
+                    if(confirm("Are you sure you want to delete this?"))
+                    {
+                        $.ajax({
+                            url:"users/deletingQuestions",
+                            method:"POST",
+                            data:{id:id},
+                            dataType:"text",
+                            success:function(data){
+                                alert(data);
+                                fetch_data();
+                            }
+                        });
                     }
                 });
             });
-        });
-    </script>
+        </script>
+
+        <!--answer-->
+        <div class="table-responsive">
+            <h3 align="center">Lisa, kustuta ja muuda vastust</h3><br />
+            <div id="live_data3"></div>
+        </div>
+
+        <script>
+            $(document).ready(function(){
+                function fetch_data()
+                {
+                    $.ajax({
+                        url:"users/selectingAnswers",
+                        method:"POST",
+                        success:function(data){
+                            $('#live_data3').html(data);
+                        }
+                    });
+                }
+                fetch_data();
+                $(document).on('click', '#btn_add3', function(){
+                    var question_id = $('#question_id').text();
+                    var answer_text = $('#answer_text').text();
+                    var answer = $('#answer').text();
+                    if(question_id == '')
+                    {
+                        alert("Enter Question Id");
+                        return false;
+                    }
+                    if(answer_text == '')
+                    {
+                        alert("Enter text");
+                        return false;
+                    }
+                    if(answer == '')
+                    {
+                        alert("Enter text");
+                        return false;
+                    }
+
+
+                    $.ajax({
+                        url:"users/insertingAnswers",
+                        method:"POST",
+                        data:{question_id:question_id, answer_text:answer_text, answer:answer},
+                        dataType:"text",
+                        success:function(data)
+                        {
+                            alert(data);
+                            fetch_data();
+                        }
+                    })
+                });
+                function edit_data(id, text, column_name)
+                {
+                    $.ajax({
+                        url:"users/editingAnswers",
+                        method:"POST",
+                        data:{id:id, text:text, column_name:column_name},
+                        dataType:"text",
+                        success:function(data){
+                            alert(data);
+                        }
+                    });
+                }
+
+                $(document).on('blur', '.question_id', function(){
+                    var id = $(this).data("id1");
+                    var text = $(this).text();
+                    edit_data(id, question_id, "question_id");
+                });
+
+                $(document).on('blur', '.answer_text', function(){
+                    var id = $(this).data("id2");
+                    var text = $(this).text();
+                    edit_data(id, answer_text, "answer_text");
+                });
+                $(document).on('blur', '.answer', function(){
+                    var id = $(this).data("id3");
+                    var text = $(this).text();
+                    edit_data(id, answer, "answer");
+                });
+
+                $(document).on('click', '.btn_delete3', function(){
+                    var id=$(this).data("id4");
+                    if(confirm("Are you sure you want to delete this?"))
+                    {
+                        $.ajax({
+                            url:"users/deletingAnswers",
+                            method:"POST",
+                            data:{id:id},
+                            dataType:"text",
+                            success:function(data){
+                                alert(data);
+                                fetch_data();
+                            }
+                        });
+                    }
+                });
+            });
+        </script>
 
     <div class="table-responsive">
         <h3 align="center">Lisa, kustuta ja muuda praktilist ülesannet</h3><br />
