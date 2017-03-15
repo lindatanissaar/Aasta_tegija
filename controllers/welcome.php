c288092671f24db33aca4a384faece40f136f8e1
Peeter <peeter.pihus@khk.ee>
2017-03-15 15:43:26 +0200

    wip

--- controllers/welcome.php
+++ controllers/welcome.php
@@ -1,7 +1,5 @@
 <?php namespace Halo;
 
-use \Aastategija\Tests;
-
 class welcome extends Controller
 {
     public $requires_auth = false;
@@ -9,51 +7,29 @@ class welcome extends Controller
 
     function index()
     {
-        $foo = Tests::add();
-        $this->forms = get_all("SELECT * FROM forms");
+        $this->users = get_all("SELECT * FROM users");
     }
 
-    function view()
-    {
-        $form_id = $this->params[0];
-        $this->form = get_first("SELECT * FROM forms WHERE form_id = '{$form_id}'");
-    }
-
-    function edit()
-    {
-        $form_id = $this->params[0];
-        $this->form = get_first("SELECT * FROM forms WHERE form_id = '{$form_id}'");
-    }
-
-    function post_edit()
-    {
-        $data = $_POST['data'];
-        insert('form', $data);
-    }
-
-    function ajax_delete()
-    {
-        exit(q("DELETE FROM forms WHERE form_id = '{$_POST['form_id']}'") ? 'Ok' : 'Fail');
-    }
 
     function AJAX_enterantsLogin()
     {
-
-//        if (isset($_POST["pin"])) {
-//            exit('pin missing');
-//        }
-
-        $pin = $_POST["pin"];
-        $user = get_first("SELECT * FROM `users` WHERE `pin` = '{$pin}'");
-
-        if (empty($user)) {
-
-            exit("Invalid user");
+        if (isset($_POST["pin"])) {
+            $pin = $_POST["pin"];
+            $conn = mysqli_connect("127.0.0.1", "root", "", "aastategija");
+            $data = mysqli_query($conn, "SELECT * FROM `users` WHERE `pin` = '{$pin}'");
+            $row_cnt = mysqli_num_rows($data);
+            if ($row_cnt == 1) {
+                echo "success";
+            }else{
+                echo "failed";
+            }
         }
+    }
 
-        $_SESSION['user_id'] = $user['user_id'];
-
-        exit("success");
+    function POST_index()
+    {
+        echo "\$_POST:<br>";
+        var_dump($_POST);
     }
 
 }
